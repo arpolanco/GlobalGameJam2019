4d0b667ec4e1c1015cd3673045c8ca393c3c9b5b
Austin Ferguson <Austin Ferguson>
2019-01-26 16:57:15 -0500

    Made battle system slightly more robust. Monsters are spawned from player and enemy party scripts.

---
+++ GobalGameJam2019/Assets/Scripts/Battle/CameraSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpin : MonoBehaviour
+{
+    public float spinFactor = 1;
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), Time.deltaTime * spinFactor);
+    }
+}
