a93f48d55c12b76dc1d58238f79f1dfa882f3a17
Austin Ferguson <Austin Ferguson>
2019-01-26 01:20:48 -0500

    Pushing skeleton code for moves, as well as some general code cleanup (e.g. reorganizing code files.)

--- GobalGameJam2019/Assets/Scripts/Dialogue/DialogueVerifier.cs
+++ GobalGameJam2019/Assets/Scripts/Dialogue/DialogueVerifier.cs
@@ -11,38 +11,38 @@ public class DialogueVerifier
         //Get prompts
         IEnumerable<XElement> prompts = tree.Descendants().Where(x => x.Name == "prompt");
         if (prompts.Count() <= 0)
-            return "No prompts found! At least one prompt is necessary!";
+            return filename + ": No prompts found! At least one prompt is necessary!";
 
         //Make sure prompts all have id and text
         foreach (XElement prompt in prompts)
         {
             if (prompt.Attribute("id") == null)
-                return "Prompt found without an id. All prompts must have an id!";
+                return filename + ": Prompt found without an id. All prompts must have an id!";
             if (prompt.Attribute("text") == null)
-                return "Prompt with id \"" + prompt.Attribute("id").Value + "\" does not have a text attribute. All prompts must have a text attribute.";
+                return filename + ": Prompt with id \"" + prompt.Attribute("id").Value + "\" does not have a text attribute. All prompts must have a text attribute.";
             string id = prompt.Attribute("id").Value;
             if (prompts.Where(x => x.Attribute("id") != null && x.Attribute("id").Value == id).Count() > 1)
-                return "Multiple prompts with id = " + id + "!";
+                return filename + ": Multiple prompts with id = " + id + "!";
 
         }
 
         //Check for root
         IEnumerable<XElement> roots = prompts.Where(x => x.Attribute("id").Value == "root");
         if (roots.Count() <= 0)
-            return "Root prompt not found. Need one prompt with id=\"root\"!";
+            return filename + ": Root prompt not found. Need one prompt with id=\"root\"!";
         if (roots.Count() > 1)
-            return "Multiple roots found! Only one root is allowed!";
+            return filename + ": Multiple roots found! Only one root is allowed!";
 
         foreach (XElement prompt in prompts)
         {
             if (prompt.Descendants().Count() <= 0)
-                return "Prompt with id \"" + prompt.Attribute("id").Value + "\" has no children. Prompts must have at least one child option element.";
+                return filename + ": Prompt with id \"" + prompt.Attribute("id").Value + "\" has no children. Prompts must have at least one child option element.";
             foreach(XElement child in prompt.Descendants())
             {
                 if (child.Name != "option")
-                    return "Non-option child of prompt detected. All child elements of prompts should be options. Prompt id = \"" + prompt.Attribute("id").Value + "\"";
+                    return filename + ": Non-option child of prompt detected. All child elements of prompts should be options. Prompt id = \"" + prompt.Attribute("id").Value + "\"";
                 if (child.Attribute("action") == null)
-                    return "Option without action detected. Prompt id = \"" + prompt.Attribute("id").Value + "\"";
+                    return filename + ": Option without action detected. Prompt id = \"" + prompt.Attribute("id").Value + "\"";
                 string action = child.Attribute("action").Value;
                 if (action == "exit")
                 { }
@@ -51,13 +51,13 @@ public class DialogueVerifier
                 else if (action == "prompt")
                 {
                     if (child.Attribute("prompt") == null)
-                        return "Child with prompt action has no prompt value! Prompt id = \"" + prompt.Attribute("id").Value + "\".";
+                        return filename + ": Child with prompt action has no prompt value! Prompt id = \"" + prompt.Attribute("id").Value + "\".";
                     string target = child.Attribute("prompt").Value;
                     if (prompts.Where(x => x.Attribute("id") != null && x.Attribute("id").Value == target).Count() <= 0)
-                        return "Child of prompt \"" + prompt.Attribute("id").Value + "\" has target of \"" + target + "\", which is not defined.";
+                        return filename + ": Child of prompt \"" + prompt.Attribute("id").Value + "\" has target of \"" + target + "\", which is not defined.";
                 }
                 else
-                    return "Unknown action provided. Prompt id = \"" + prompt.Attribute("id").Value + "\". Action = \"" + action + "\".";
+                    return filename + ": Unknown action provided. Prompt id = \"" + prompt.Attribute("id").Value + "\". Action = \"" + action + "\".";
             }
         }
 
---
+++ GobalGameJam2019/Assets/Scripts/Moves/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveClassification {DAMAGE, STATUS, BUFF}
+
+[System.Serializable]
+public struct MoveEffect
+{
+    public Element element;
+    public MoveClassification classification;
+    public float quantVal;
+    public string qualVal;
+}
+
+
+public class Move : ScriptableObject
+{
+    public string id, animation, particleEffect;
+    public List<MoveEffect> effects;
+}
