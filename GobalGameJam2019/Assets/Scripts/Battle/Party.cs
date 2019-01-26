229afd31995d3e0ddbc33e73e05a2916ba55eaaf
JalenTheEpic <jalencureton@live.com>
2019-01-26 12:46:53 -0500

    MONSTER MASH

---
+++ GobalGameJam2019/Assets/Scripts/Battle/Party.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Party : MonoBehaviour
+{
+
+    [SerializeField]
+    public int[] mActiveParty;
+
+
+    public List<GameObject> mOwnedMonsters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mOwnedMonsters = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
