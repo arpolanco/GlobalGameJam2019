b4904161ee5182a0c2f8591e0e1c93b0837ab97a
daltonchutes <daltonchts427@gmail.com>
2019-01-26 00:02:13 -0600

    committing suicide

---
+++ GobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        //playerAnimator.Play("HumanoidWalk");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
