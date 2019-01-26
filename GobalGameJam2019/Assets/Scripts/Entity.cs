49b09b7c3a489bd77fdc9332cc2934969d825e53
Ian Woodley <ianwoodley@sbcglobal.net>
2019-01-25 22:17:08 -0500

    Base Entity skeleton.

---
+++ GobalGameJam2019/Assets/Scripts/Entity.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+/*
+physical dmg
+magic dmg
+HP
+MP
+
+playAttackAnimation()
+*/
+
+public class Entity : MonoBehaviour
+{
+    private int hp, mp;
+
+    public int maxHp, maxMp;
+    public int physicalDamage;
+    public int magicDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = maxHp;
+        mp = maxMp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playAnimation()
+    {
+        // TODO
+    }
+}
