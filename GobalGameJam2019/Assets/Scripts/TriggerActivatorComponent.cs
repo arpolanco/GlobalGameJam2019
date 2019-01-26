7d6cc1f595944e8275347030264042db29083b54
daltonchutes <daltonchts427@gmail.com>
2019-01-25 23:21:46 -0600

    basic trigger-event system

---
+++ GobalGameJam2019/Assets/Scripts/TriggerActivatorComponent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerActivatorComponent : MonoBehaviour
+{
+    [SerializeField]
+    private bool isButtonTrigger = false;
+
+    private BoxCollider boxTrigger;
+
+    private bool isPlayerInTrigger = false;
+    
+    private GameEventComponent gameEventComp;
+
+
+    // TODO: Won't need this later. Keeping it to retain logic. Replace with Input later
+    private bool hasHitButton = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<BoxCollider>() == null)
+            isPlayerInTrigger = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInTrigger)
+        {
+            if (isButtonTrigger && hasHitButton)
+            {
+                GetComponent<GameEventComponent>();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+}
