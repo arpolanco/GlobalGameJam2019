f62c1344e1de37d6e8a75961d7edacf358fa81c9
JalenTheEpic <jalencureton@live.com>
2019-01-27 13:46:05 -0500

    Add party UI

---
+++ GobalGameJam2019/Assets/Scripts/UI/PartyUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class PartyUI : MonoBehaviour
+{
+
+    GameObject[] slots;
+    GameObject[] partyslots;
+
+    Party party;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        slots = GameObject.FindGameObjectsWithTag("MonsterSlot");
+        partyslots = GameObject.FindGameObjectsWithTag("PartySlot");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+ 
+}
+
