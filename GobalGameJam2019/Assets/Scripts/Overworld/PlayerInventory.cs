4551135dd7f89139aafbfba2b5f2e6353f307a37
Andrew Polanco <loopypup@gmail.com>
2019-01-26 18:48:27 -0600

    HELP
    HELPELPELEPLEPLPELEPLP

---
+++ GobalGameJam2019/Assets/Scripts/Overworld/PlayerInventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    //Misc Player Stuff that you dont know where to put it
+    public int moneyPouch = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addMoney(int gold)
+    {
+        moneyPouch += gold;
+        if (moneyPouch > 99999)
+            moneyPouch = 99999;
+    }
+
+    public void subMoney(int gold)
+    {
+        moneyPouch -= gold;
+        if (moneyPouch <= 0)
+            moneyPouch = 0;
+    }
+}
